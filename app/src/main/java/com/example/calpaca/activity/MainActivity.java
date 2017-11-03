6285ea56653f5f71290a9290ae0bd3dba9fa9c5b
mingyanisa <mingignim@hotmail.com>
2017-11-03 16:12:53 +0700

    main menu

--- app/src/main/java/com/example/calpaca/activity/MainActivity.java
+++ app/src/main/java/com/example/calpaca/activity/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.calpaca.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.calpaca.R;
 import com.example.calpaca.fragment.MainFragment;
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         initInstance(savedInstanceState);
--- app/src/main/java/com/example/calpaca/fragment/CameraFragment.java
+++ app/src/main/java/com/example/calpaca/fragment/CameraFragment.java
@@ -4,18 +4,28 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Camera;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.calpaca.R;
+import com.varunest.sparkbutton.SparkButton;
+import com.varunest.sparkbutton.SparkButtonBuilder;
+import com.varunest.sparkbutton.SparkEventListener;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by admin on 30/10/2017 AD.
@@ -26,8 +36,9 @@ public class CameraFragment extends Fragment {
     private static CameraFragment instance;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int PICK_IMAGE = 2;
+    private static SparkButton btnSelectPhoto;
+    private static SparkButton btnTakePhoto;
 
-    private Button btnTakePhoto, btnSelectPhoto;
     private ImageView mImageView;
 
     public static CameraFragment newInstance() {
@@ -43,6 +54,7 @@ public class CameraFragment extends Fragment {
         initFragment(savedInstanceState);
     }
 
+
     private void initFragment(Bundle savedInstanceState) {
 
     }
@@ -56,24 +68,59 @@ public class CameraFragment extends Fragment {
     }
 
     private void initInstance(View rootView, Bundle savedInstanceState) {
-        btnTakePhoto = (Button) rootView.findViewById(R.id.btnTakePhoto);
-        btnTakePhoto.setOnClickListener(takePhotoListener);
 
-        btnSelectPhoto = (Button) rootView.findViewById(R.id.btnSelectPhoto);
-        btnSelectPhoto.setOnClickListener(selectPhotoListener);
+        btnTakePhoto = (SparkButton) rootView.findViewById(R.id.btnTakePhoto);
+        btnSelectPhoto = (SparkButton) rootView.findViewById(R.id.btnSelectPhoto);
+        btnTakePhoto.setEventListener(new SetSpark(btnTakePhoto));
+        btnSelectPhoto.setEventListener(new SetSpark(btnSelectPhoto));
 
-        mImageView = (ImageView) rootView.findViewById(R.id.mImageView);
+
+        final Animation myAnim = AnimationUtils.loadAnimation(getContext(), R.anim.bouncing);
+
+
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+        myAnim.setInterpolator(interpolator);
+        btnTakePhoto.startAnimation(myAnim);
+        btnSelectPhoto.startAnimation(myAnim);
+
+
+
     }
 
-    View.OnClickListener takePhotoListener = new View.OnClickListener() {
+
+    class SetSpark implements SparkEventListener{
+        private SparkButton btn;
+        SetSpark(SparkButton btn){
+            this.btn= btn;
+
+        }
         @Override
-        public void onClick(View v) {
-            if (v == btnTakePhoto) {
+        public void onEvent(ImageView button, boolean buttonState) {
+            if(btn == btnTakePhoto){
+                Log.d("SparkButton", "In listener");
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(takePictureIntent, 1);
+
+            }
+            if(btn == btnSelectPhoto){
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
             }
         }
-    };
+
+        @Override
+        public void onEventAnimationEnd(ImageView button, boolean buttonState) {
+
+        }
+
+        @Override
+        public void onEventAnimationStart(ImageView button, boolean buttonState) {
+
+        }
+    }
+
 
     View.OnClickListener selectPhotoListener = new View.OnClickListener() {
         @Override
@@ -99,6 +146,20 @@ public class CameraFragment extends Fragment {
 //            Bundle extras = data.getExtras();
 //            Bitmap imageBitmap = (Bitmap) extras.get("data");
 //            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+    class MyBounceInterpolator implements android.view.animation.Interpolator {
+        private double mAmplitude = 0.1;
+        private double mFrequency = 10;
+
+        MyBounceInterpolator(double amplitude, double frequency) {
+            mAmplitude = amplitude;
+            mFrequency = frequency;
+        }
+
+        public float getInterpolation(float time) {
+            return (float)(-1 * Math.pow(Math.E, -time/ mAmplitude) *
+                    Math.cos(mFrequency * time) + 1);
         }
     }
 }
